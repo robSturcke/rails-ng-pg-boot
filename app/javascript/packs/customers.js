3d378965667bccd512436f388307f18df7363c30
Rob Sturcke <robSturcke@users.noreply.github.com>
2017-08-25 08:56:16 -0400

    imports for angular

---
+++ app/javascript/packs/customers.js
@@ -0,0 +1,6 @@
+import "hello_angular/polyfills";
+
+import { Component, NgModule    } from "@angular/core";
+import { BrowserModule          } from "@angular/platform-browser";
+import { FormsModule            } from "@angular/forms";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
