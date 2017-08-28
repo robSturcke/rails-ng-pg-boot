d8b48fcf6cadc62a204f043d09f76232247a3976
Rob Sturcke <robSturcke@users.noreply.github.com>
2017-08-28 14:11:50 -0400

    customer data for angular

--- app/javascript/packs/customers.js
+++ app/javascript/packs/customers.js
@@ -4,6 +4,47 @@ import { Component, NgModule    } from "@angular/core";
 import { BrowserModule          } from "@angular/platform-browser";
 import { FormsModule            } from "@angular/forms";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { Http,HttpModule        } from "@angular/http";
+
+var RESULTS = [
+  {
+    first_name: "Pat",
+    last_name: "Smith",
+    username: "psmith",
+    email: "pat.smith@somewhere.net",
+    created_at: "2016-02-05",
+  },
+  {
+    first_name: "Patrick",
+    last_name: "Jones",
+    username: "pjpj",
+    email: "jones.p@business.net",
+    created_at: "2014-03-05",
+  },
+  {
+    first_name: "Patricia",
+    last_name: "Benjamin",
+    username: "pattyb",
+    email: "benjie@aol.info",
+    created_at: "2016-01-02",
+  },
+  {
+    first_name: "Patty",
+    last_name: "Patrickson",
+    username: "ppat",
+    email: "pppp@freemail.computer",
+    created_at: "2016-02-05",
+  },
+  {
+    first_name: "Jane",
+    last_name: "Patrick",
+    username: "janesays",
+    email: "janep@company.net",
+    created_at: "2013-01-05",
+  },
+];
+
+
 
 var CustomerSearchComponent = Component({
   selector: "shine-customer-search",
@@ -16,12 +57,13 @@ var CustomerSearchComponent = Component({
     <div class="input-group input-group-lg"> \
       <label for="keywords" class="sr-only">Keywords></label> \
       <input type="text" id="keywords" name="keywords" \
-             placeholder="First Name, Last Name, or Email Address" \
+             placeholder="First Name, Last Name, or Email Address"\
              class="form-control input-lg" \
-             bindon-ngModel="keywords">\
+             bindon-ngModel="keywords"> \
       <span class="input-group-btn"> \
-        <input type="submit" value="Find Customers" \
-               class="btn btn-primary btn-lg"> \
+        <input type="submit" value="Find Customers"\
+         class="btn btn-primary btn-lg" \
+         on-click="search()"> \
       </span> \
     </div> \
   </form> \
@@ -32,7 +74,7 @@ var CustomerSearchComponent = Component({
   </header> \
   <ol class="list-group"> \
     <li *ngFor="let customer of customers" \
-        class="list-group-item clearfix"> \
+      class="list-group-item clearfix"> \
       <h3 class="pull-right"> \
         <small class="text-uppercase">Joined</small> \
         {{customer.created_at}} \
@@ -47,16 +89,34 @@ var CustomerSearchComponent = Component({
 </section> \
   '
 }).Class({
-  constructor: function() {
-    this.keywords = null;
-  },
+  constructor: [
+    Http,
+    function(http) {
+      this.customers = null;
+      this.http      = http;
+      this.keywords  = "";
+    }
+  ],
   search: function() {
-    alert("Searched for: " + this.keywords);
+    var self = this;                                // (1)
+    self.http.get(                                  // (2)
+      "/customers.json?keywords=" + self.keywords   // (3)
+    ).subscribe(
+      function(response) {                          // (4)
+        self.customers = response.json().customers; // (5)
+      },
+      function(response) {                          // (6)
+        window.alert(response);
+      }
+    );
   }
 });
 
 var CustomerAppModule = NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    HttpModule ],
   declarations: [ CustomerSearchComponent ],
   bootstrap:    [ CustomerSearchComponent ]
 })
