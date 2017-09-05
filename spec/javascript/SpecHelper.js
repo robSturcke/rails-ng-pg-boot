dff46043bc6e5368e964527fbbf0546642a4dbaa
Rob Sturcke <robSturcke@users.noreply.github.com>
2017-09-05 15:49:15 -0400

    specHelper test

--- spec/javascript/SpecHelper.js
+++ spec/javascript/SpecHelper.js
@@ -6,4 +6,13 @@ describe("CustomerSearchComponent", function() {
   beforeEach(function() {
     component = new CustomerSearchComponent();
   });
+
+  describe("initial state", function() {
+    it("sets customers to null", function() {
+      expect(component.customers).toBe(null);
+    });
+    it("sets keywords to the empty string", function() {
+      expect(component.keywords).toBe("");
+    });
+  });
 });
