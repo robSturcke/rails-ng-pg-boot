c166ef7baa0ab4ec8e2f88c557ccfb168e7251ab
Rob Sturcke <robSturcke@users.noreply.github.com>
2017-09-11 15:38:19 -0400

    first test that asserts keywords props get set to search string when calling search
    also test that requires that the get method on http wasn't called to
    assert that a search for a string shorter than 3char doesn't process

--- spec/javascript/CustomerSearchComponent.spec.js
+++ spec/javascript/CustomerSearchComponent.spec.js
@@ -15,8 +15,14 @@ describe("CustomerSearchComponent", function() {
       component = new CustomerSearchComponent(mockHttp);
     });
     describe("A search for 'pa', less than three characters", function() {
-      it("sets the keywords to be 'pa'");
-      it("does not make an HTTP call");
+      it("sets the keywords to be 'pa'", function() {
+        component.search("pa");
+        expect(component.keywords).toBe("pa");
+      });
+      it("does not make an HTTP call", function() {
+        component.search("pa");
+        td.verify(mockHttp.get(), { times: 0 });
+      });
     });
     describe("A search for 'pat', three or more characters", function() {
       describe("A successful search", function() {
