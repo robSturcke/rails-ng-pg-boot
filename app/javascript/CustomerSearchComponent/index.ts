93cb86fb7e62cf34d56bab05f6410cd2af7d8af1
Rob Sturcke <robSturcke@users.noreply.github.com>
2017-09-12 10:11:12 -0400

    imoprt template

--- app/javascript/CustomerSearchComponent/index.ts
+++ app/javascript/CustomerSearchComponent/index.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Http      } from "@angular/http";
+import template from "./template.html";
 
 var CustomerSearchComponent = Component({
   selector: "shine-customer-search",
