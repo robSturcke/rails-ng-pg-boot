72f0c20e475b2a983869d391fc9c07ab3374f9d0
Rob Sturcke <robSturcke@users.noreply.github.com>
2017-09-12 12:26:00 -0400

    spacing fixes

--- app/javascript/CustomerSearchComponent/index.ts
+++ app/javascript/CustomerSearchComponent/index.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { Http      } from "@angular/http";
-import template from "./template.html";
+import   template    from "./template.html";
 
 var CustomerSearchComponent = Component({
+
   selector: "shine-customer-search",
   template: template
-  }).Class({
+}).Class({
   constructor: [
     Http,
     function(http) {
